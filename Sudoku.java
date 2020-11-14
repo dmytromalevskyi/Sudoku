4d8ff18342b74371c7001f20eeb383b4c065a193
Dmytro Malevskyi <dmytromalevskyi@gmail.com>
2020-11-14 14:42:26 +0000

    possible to generate solved 2*2 and 3*3 boards and a logo was added

--- Sudoku.java
+++ Sudoku.java
@@ -1,16 +1,41 @@
 import java.lang.Math;
 import java.util.Scanner;
+import java.util.Arrays;
 
 
 class Sudoku{
     public static void main(String[] args) {
-        byte[][] board = Board.generate2DArrays(2);
-        Board.draw(board);
-      
+        printLogo();
+        int level = 2;
+        byte[][] board = Board.generate2DArrays(level);
+        //Board.draw(board);
+        
         board = Board.solveSudoku(board);
         Board.draw(board);
 
         
+    }
+
+    // Print a cool Sudoku logo
+    //
+    public static void printLogo() {
+        System.out.println();
+        System.out.println("    sSSs   .S       S.    .S_sSSs      sSSs_sSSs     .S    S.    .S       S.   ");
+        System.out.println("   d%%SP  .SS       SS.  .SS~YS%%b    d%%SP~YS%%b   .SS    SS.  .SS       SS.  ");
+        System.out.println("  d%S'    S%S       S%S  S%S   `S%b  d%S'     `S%b  S%S    S&S  S%S       S%S  ");
+        System.out.println("  S%|     S%S       S%S  S%S    S%S  S%S       S%S  S%S    d*S  S%S       S%S  ");
+        System.out.println("  S&S     S&S       S&S  S%S    S&S  S&S       S&S  S&S   .S*S  S&S       S&S  ");
+        System.out.println("  Y&Ss    S&S       S&S  S&S    S&S  S&S       S&S  S&S_sdSSS   S&S       S&S  ");
+        System.out.println("  `S&&S   S&S       S&S  S&S    S&S  S&S       S&S  S&S~YSSY%b  S&S       S&S  ");
+        System.out.println("    `S*S  S&S       S&S  S&S    S&S  S&S       S&S  S&S    `S%  S&S       S&S  ");
+        System.out.println("     l*S  S*b       d*S  S*S    d*S  S*b       d*S  S*S     S%  S*b       d*S  ");
+        System.out.println("    .S*P  S*S.     .S*S  S*S   .S*S  S*S.     .S*S  S*S     S&  S*S.     .S*S  ");
+        System.out.println("  sSS*S    SSSbs_sdSSS   S*S_sdSSS    SSSbs_sdSSS   S*S     S&   SSSbs_sdSSS   ");
+        System.out.println("  YSS'      YSSP~YSSY    SSS~YSSY      YSSP~YSSY    S*S     SS    YSSP~YSSY    ");
+        System.out.println("                                                    SP                         ");
+        System.out.println("                                                    Y                          ");
+        System.out.println();
+                                                                                       
     }
 }
 
@@ -82,43 +107,87 @@ class Board{
         System.out.println(bottomBorder);
     }
 
-    // Return a solved 2D array with the size of the passed one
+    // Randomly generate values for a board until one that is valid is optained
     //
     public static byte[][] solveSudoku(byte[][] board) {
+        int level = (int) Math.sqrt(board.length);
+
+        while(Board.are2DByteArraysEqual(Board.generate2DArrays(level), board)){
+            board = Board.gererateRandomValues(board);
+        }
+
+        return board;
+    }
+
+    // Return a solved 2D array with the size of the passed one
+    //
+    public static byte[][] gererateRandomValues(byte[][] board) {
         int dimention = board.length;
         int level = (int) Math.sqrt(board.length);
 
-        // Generate the top left box
-        byte index = 1;
-        while (index <= dimention){
-            for (int i = 0; i <= level-1; i++){
-                for (int z = 0; z <= level-1; z++){
-                    board[i][z] = index;
-                    index++;
+        // Sometimes there will be no byte that can pe places in the next squere (due to the method used) so everything should start all over again
+        boolean invalidSudoku = false;
+        
+        int x = 0;
+        while (x <= dimention-1){
+            int boxX = (int) Math.floor(x / level) * level;
+
+            int y = 0;
+            while (y <= dimention-1){
+                int boxY = (int) Math.floor(y / level) * level;
+
+                boolean foundNumber = false;
+                byte[] currentRow = board[x];
+                byte[] currentColoumn = getColoumnAsArray(board, y);
+                byte[] currentBox = getBoxAsArray(board, boxX, boxY);
+
+                /*
+                System.out.println("Current x,y: "+x+","+y);
+                System.out.println("Current boxX,boxY: "+boxX+","+boxY);
+                System.out.println("Current Box");
+                for (byte num : currentBox){
+                    System.out.println(num);
                 }
+                System.out.println("End of Current Box");
+                draw(board);
+                System.out.println();
+                */
+
+
+                int count = 0;
+                while (!foundNumber){
+                    byte randomNumber = getRandomNumber((byte) 1,(byte) dimention);
+                    if ( !isInArray(currentRow, randomNumber) & !isInArray(currentColoumn, randomNumber) & !isInArray(currentBox, randomNumber) ){
+                        board[x][y] = randomNumber;
+                        foundNumber = true;
+                    } else if (invalidSudoku){
+                        break;
+                    } else{
+                        count++;
+                        invalidSudoku = (count >= dimention*2) ? true : false;
+                    }
+
+                }
+                y++;
+                if (invalidSudoku)
+                    break;
             }
-        }
-
-        // Generate the rest of the coloumn zero
-        byte [] alreadyInColoumn = new byte[level];
-        for (int i = 0; i <= level-1; i++){
-            alreadyInColoumn[i] = board[i][0];
-        }
-        byte [] notYetInColoumn = getUncommonElements(dimention, alreadyInColoumn);
-        for (int i = level; i <= dimention-1; i++){
-            board[i][0] = notYetInColoumn[i-level];
-        }
-
-        // Generate the rest of the row zero
-        byte [] alreadyInRow = new byte[level];
-        for (int i = 0; i <= level-1; i++){
-            alreadyInRow[i] = board[0][i];
-        }
-        byte [] notYetInRow = getUncommonElements(dimention, alreadyInRow);
-        for (int i = level; i <= dimention-1; i++){
-            board[0][i] = notYetInRow[i-level];
+            x++;
+            if (invalidSudoku)
+                    break;
         }
         
+        if (invalidSudoku){
+            return generate2DArrays(level);
+            /* Original way with StackOverglowError
+            // If the board generated is invalid just start all over again
+            board = generate2DArrays(level);
+            //System.out.println("The programme failed and the solving is being restarted...");
+            board = gererateRandomValues(board);
+            */
+        }
+
+
         return board;
     }
 
@@ -126,16 +195,7 @@ class Board{
     // coloumn and row indexing the top left corner of the box
     //
     public static boolean isBoxDistinct(byte[][] board, int row, int coloumn){
-        int level = (int) Math.sqrt(board.length);
-        byte[] byteArray = new byte[board.length];
-        int index = 0;
-        
-        for (int i = coloumn; i <= coloumn+level-1; i++){
-            for (int z = row; z <= row+level-1; z++){
-                byteArray[index] = board[i][z];
-                index++;
-            }
-        }
+        byte[] byteArray = getBoxAsArray(board, row, coloumn);
         return isByteArrayDistinct(byteArray);
     }
     
@@ -175,6 +235,35 @@ class Board{
         }
 
         return isByteArrayDistinct(byteArray);
+    }
+
+    // Returns a byte array of the items of a 2D byte array
+    //
+    public static byte[] getColoumnAsArray(byte[][] board, int coloumn) {
+        byte [] coloumnArray = new byte[board.length];
+        
+        for (int i = 0; i <= board.length-1; i++){
+            coloumnArray[i] = board[i][coloumn];
+        }
+
+        return coloumnArray;
+    }
+
+    // Return the elements of the box in a single byte array
+    // coloumn and row indexing the top left corner of the box
+    //
+    public static byte[] getBoxAsArray(byte[][] board, int row, int coloumn){
+        int level = (int) Math.sqrt(board.length);
+        byte[] byteArray = new byte[board.length];
+        int index = 0;
+        
+        for (int i = row; i <= row+level-1; i++){
+            for (int z = coloumn; z <= coloumn+level-1; z++){
+                byteArray[index] = board[i][z];
+                index++;
+            }
+        }
+        return byteArray;
     }
     
     // Generate top border for n level
@@ -239,8 +328,8 @@ class Board{
 
     // Generate a random number within a range
     //
-    public int getRandomNumber(int min, int max) {
-        return (int) ((Math.random() * (max - min)) + min);
+    public static byte getRandomNumber(byte min, byte max) {
+        return (byte) ((Math.random() * (max + 1 - min)) + min);
     }
 
     // Return array of uncommon elements between [1,2,3 .. max] and byteArray
@@ -274,7 +363,34 @@ class Board{
         return uncommonElements;
     }
 
-    // Return a string with appropriate number of space
+    // Return if a number is in the array
+    //
+    public static boolean isInArray(byte[] byteArray, byte userNumber) {
+        boolean isFound = false;
+        
+        for (byte number : byteArray){
+            if (number == userNumber){
+                isFound = true;
+                break;
+            }
+        }
+        return isFound;
+    }
+
+    // Cheack if 2 2D byte arrays are equal
+    //
+    public static boolean are2DByteArraysEqual(byte[][] first, byte[][] second) {
+        boolean areDistinct = true;
+        for (int i = 0; i <= first.length-1; i++){
+            if (!Arrays.equals(first[i], second[i])){
+                areDistinct = false;
+                break;
+            }
+        }
+        return areDistinct;
+    }
+
+    // Return a string that will have the unit repeated the number of digits dimention contains 
     //
     public static String createRepeatedUnit(int dimention, String unit) {
         String unitInstance = unit;
@@ -288,8 +404,10 @@ class Board{
         for (byte i = 1; i <= widthOfCharacters-1; i++){
             unit = unit+unitInstance;
         }
+        /*
         System.out.println("The number of digits is: "+widthOfCharacters);
         System.out.println("\""+unit+"\"");
+        */
         return unit;
     }
 }
